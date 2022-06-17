c1a0fe72401a9a619e3a1652454f5fa53d4412b1
jesserubic <jesserubic@gmail.com>
2022-06-18 03:31:48 +0800

    Interactions

---
+++ story_content/5W6VsxYMMgi_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.64.27845.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.306%0D%0A%20Congratulations!%20Thank%20you%20for%20taking%20this%20course.%20%20%20%20%20%20%20%0D%0A%0D%0A"}')
---
+++ story_content/5xDacTvAtFO_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.64.27845.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.248%0D%0ACongratulations,%20you%20picked%20the%20fresh%20eggs!%20%20%20%0D%0A%0D%0A"}')
